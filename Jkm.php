8fbdd880bf7ec8ff4a99b422e4d98e76df39cf49
BrianVega <114993494+BrianRVP@users.noreply.github.com>
2024-05-14 23:51:38 -0300

    Update Jkm.php

--- Jkm.php
+++ Jkm.php
@@ -1,3 +1,3 @@
 <datos> LOGO:"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/daAg63wSNiJ9GLs99qP3KKL7TiZ.jpg" GRUPO:"ESTRENOS CAM" NOMBRE:"Super Mario Bros. La pelicula (2023) HD GD
-"URL:"https://vod-normal-global-cdn-z02.afreecatv.com/v101/afflv14/_definst_/smil:save/afreeca/station/2023/0522/05/1684702763612177.smil/playlist.m3u8"</datos>
+"URL:"https://www.googleapis.com/drive/v3/files/1N6KTCxEb8K4eZWWqGeKqZl6ODaoBnxqq?alt=media&key=AIzaSyDOh3TzVbUSrk1MKCPyFDt_PzcYBTVKz10"</datos>
 
